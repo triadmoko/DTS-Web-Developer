f2c571f291e834f299c7300a79426914b130ae01
triadmoko <triadmoko12@gmail.com>
2020-09-29 21:54:31 +0700

    Single and Multiple

---
+++ Tugas_pertemuan_2/tugas_1/script.js
@@ -0,0 +1,8 @@
+// Dropdown Menu Fade    
+jQuery(document).ready(function(){
+    $(".dropdown").hover(
+        function() { $('.dropdown-menu', this).fadeIn("fast");
+        },
+        function() { $('.dropdown-menu', this).fadeOut("fast");
+    });
+});
